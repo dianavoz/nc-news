dc4b6cdea6fe4c1fa5cdfbba049e0f225a29b633
Diana Vozanu <you@example.com>
2022-04-20 22:08:38 +0100

    function to get topics

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,8 +4,20 @@ const articlesApi = axios.create({
   baseURL: "https://api-backend-nc-news.herokuapp.com/api",
 });
 
-export const getArticles = () => {
-  return articlesApi.get("/articles").then(({ data }) => {
-    return data.articles;
+export const getTopicsApi = () => {
+  return articlesApi.get("/topics").then(({ data }) => {
+    return data.topics;
   });
+};
+
+export const getArticles = (topic) => {
+  return articlesApi
+    .get("/articles", {
+      params: {
+        topic,
+      },
+    })
+    .then(({ data }) => {
+      return data.articles;
+    });
 };
