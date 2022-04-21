e44272feef052524ffeab960dce64dac13b8453e
Diana Vozanu <you@example.com>
2022-04-21 11:59:21 +0100

    added article.body option to the article card

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -3,7 +3,7 @@ const Article = ({ article }) => {
     <>
       <h3>{article.title}</h3>
       <p>{article.author}</p>
-
+      {!article.body ? "" : article.body}
       <span>{article.topic}</span>
       <span>{article.created_at}</span>
     </>
