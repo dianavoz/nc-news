9af7629859865890e12cf8771467a32907332cfc
Diana Vozanu <you@example.com>
2022-04-21 18:48:19 +0100

    ncnews-23

--- src/components/ArticleVote.jsx
+++ src/components/ArticleVote.jsx
@@ -4,27 +4,31 @@ import { patchArticleVote } from "../utils/api";
 
 const ArticleVote = ({ article_id, vote }) => {
   const [votes, setVotes] = useState(0);
+  const [err, setErr] = useState(null);
 
   const incrementVoteCount = () => {
+    setErr(null);
     setVotes((currentVote) => currentVote + 1);
     patchArticleVote(article_id, vote + 1).catch(() => {
       setVotes((currentVote) => currentVote - 1);
+      setErr("Sorry, something went wrong!");
     });
   };
   const decrementVoteCount = () => {
+    setErr(null);
     setVotes((currentVote) => currentVote - 1);
     patchArticleVote(article_id, vote - 1).catch(() => {
       setVotes((currentVote) => currentVote - 2);
+      setErr("Sorry, something went wrong!");
     });
   };
 
+  if (err) return <p>err</p>;
   return (
     <div className="vote">
       <p>Votes:{vote + votes}</p>
-      <button onClick={incrementVoteCount} disabled={votes > 0}>
-        Upvote
-      </button>
-      <button onClick={decrementVoteCount}>Downvote</button>
+      <button onClick={incrementVoteCount}>Vote +</button>
+      <button onClick={decrementVoteCount}>Vote - </button>
     </div>
   );
 };
