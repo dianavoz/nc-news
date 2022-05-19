cda780d1a25aa84ff37ab35aafd1313a2b1ccbb7
Diana Vozanu <you@example.com>
2022-05-19 21:17:59 +0100

    added padding to the comment card

--- src/components/CommentsCard.jsx
+++ src/components/CommentsCard.jsx
@@ -1,15 +1,11 @@
-import { Typography } from '@mui/material';
-
 const CommentsCard = ({ comment }) => {
   return (
-    <div className='comments-card'>
-      <Typography variant='body2' color='text.secondary' textAlign='left'>
-        <p>{comment.body}</p>
-        <span>
-          <span className='comments-span-by'>by</span> {comment.author} |{' '}
-          {comment.votes} comments | {comment.created_at.slice(0, 10)}
-        </span>
-      </Typography>
+    <div className='card comments-card'>
+      <p>{comment.body}</p>
+      <span>
+        <span className='comments-span-by'>by</span> {comment.author} |{' '}
+        {comment.votes} comments | {comment.created_at.slice(0, 10)}
+      </span>
     </div>
   );
 };
