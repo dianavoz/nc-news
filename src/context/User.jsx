08181120e8297339581c23d6de28d3be36a87a12
Diana Vozanu <you@example.com>
2022-05-21 15:40:58 +0100

    removed hard coded username

--- src/context/User.jsx
+++ src/context/User.jsx
@@ -4,7 +4,7 @@ export const UserContext = createContext();
 
 export const UserProvider = (props) => {
   const [isLoggedIn, setIsLoggedIn] = useState({
-    username: 'grumpy19',
+    username: '',
   });
 
   // const isLoggedIn = user.username !== undefined;
