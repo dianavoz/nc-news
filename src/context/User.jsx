b5fef9afb6ed26efa17c6db4c64e2bcf18a76dbb
Diana Vozanu <you@example.com>
2022-05-21 15:37:17 +0100

    removed log out button and added toggle

--- src/context/User.jsx
+++ src/context/User.jsx
@@ -4,7 +4,7 @@ export const UserContext = createContext();
 
 export const UserProvider = (props) => {
   const [isLoggedIn, setIsLoggedIn] = useState({
-    username: '',
+    username: 'grumpy19',
   });
 
   // const isLoggedIn = user.username !== undefined;
